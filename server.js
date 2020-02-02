612b3346987fdd88cde668834040a425e9c4faa0
Glitch (aeolian-comic-0cu4gddmt) <none>
2020-02-02 17:52:18 +0000

    🍽📮 Checkpoint ./package.json:10752519/25 ./server.js:10752519/476

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var express = require('express');
 var cors = require('cors');
 
 // require and use "multer"...
+var multer = require('multer');
+var storage = multer.memoryStorage();
+var upload = multer({storage: storage});
 
 var app = express();
 
@@ -14,8 +17,14 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res){
+
+    if(req.file){
+        res.send({name: req.file.originalname,  size: req.file.size});
+    }else {
+        res.send({error: "no file uploaded"});
+    }
+    
 });
 
 app.listen(process.env.PORT || 3000, function () {
